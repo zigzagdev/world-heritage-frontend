50efb6940e9a0addc2b6f3a4188918ef62466cdf
Application-drop-up <msts.oo0131@gmail.com>
2025-09-27 15:10:38 +0900

    feat: conflict-fix

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,36 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import pluginHtml from 'eslint-plugin-html'
-import unusedImports from 'eslint-plugin-unused-imports'
-import prettier from 'eslint-config-prettier'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import html from "eslint-plugin-html";
+import unusedImports from "eslint-plugin-unused-imports";
+import prettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist', 'node_modules', 'coverage']),
-  pluginHtml.configs.recommended,
+  globalIgnores(["dist", "node_modules", "coverage"]),
+  {
+    files: ["**/*.html"],
+    plugins: { html },
+  },
 
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
-    plugins: { 'unused-imports': unusedImports },
+    plugins: { "unused-imports": unusedImports },
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: globals.browser,
     },
     rules: {
-      'no-warning-comments': ['warn', { terms: ['todo', 'fixme'], location: 'anywhere' }],
-      'no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
+      "no-warning-comments": [
+        "warn",
+        { terms: ["todo", "fixme"], location: "anywhere" },
+      ],
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
     },
   },
   prettier,
-])
+]);
