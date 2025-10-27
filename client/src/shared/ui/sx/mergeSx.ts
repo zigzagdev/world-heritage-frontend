525428aeb0b9a38ba7cc95e628e822707e66c165
Application-drop-up <msts.oo0131@gmail.com>
2025-10-27 14:57:51 +0900

    fix: change directory name & delete no using file

--- client/src/shared/ui/sx/mergeSx.ts
+++
@@ -1,5 +0,0 @@
-import type { SxProps, Theme } from "@mui/system";
-
-export const mergeSx = (
-  ...items: Array<SxProps<Theme> | false | null | undefined>
-): SxProps<Theme> => items.filter(Boolean) as SxProps<Theme>;
