9782ca054b2055c0a164adb44900aa72d7748090
Application-drop-up <msts.oo0131@gmail.com>
2026-03-01 17:48:52 +0900

    feat: Implement Provider & adapt it to Routing

--- client/src/app/routes/AppRoutes.tsx
+++ client/src/app/routes/AppRoutes.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import TopPageContainer from "@features/top/containers/top-page-container.tsx";
 import { WorldHeritageDetailContainer } from "@features/top/containers/world-heritage-detail-container.tsx";
 import { SearchHeritageResultsContainer } from "@features/search/containers/search-heritage-result-container.tsx";
+import { BreadcrumbProvider } from "@features/breadcrumbs/BreadCrumbProvider.tsx";
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/heritages" element={<TopPageContainer />} />
-      <Route path="/heritages/:id" element={<WorldHeritageDetailContainer />} />
-      <Route path="/heritages/results" element={<SearchHeritageResultsContainer />} />
-    </Routes>
+    <BreadcrumbProvider>
+      <Routes>
+        <Route path="/heritages" element={<TopPageContainer />} />
+        <Route path="/heritages/:id" element={<WorldHeritageDetailContainer />} />
+        <Route path="/heritages/results" element={<SearchHeritageResultsContainer />} />
+      </Routes>
+    </BreadcrumbProvider>
   );
 }
