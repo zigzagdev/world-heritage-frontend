3f55124876f0ef039d8e30a4a6ca57f2596c6844
Application-drop-up <msts.oo0131@gmail.com>
2026-03-07 19:20:35 +0900

    feat: add 404 fallback

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+const redirectPath = sessionStorage.getItem("gh-pages-spa-redirect");
+
+if (redirectPath !== null) {
+  sessionStorage.removeItem("gh-pages-spa-redirect");
+
+  const baseUrl = import.meta.env.BASE_URL.endsWith("/")
+    ? import.meta.env.BASE_URL.slice(0, -1)
+    : import.meta.env.BASE_URL;
+
+  window.history.replaceState(null, "", `${baseUrl}${redirectPath}`);
+}
+
+const rootElement = document.getElementById("root");
+
+if (rootElement === null) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
