125f13f2853ce34bfd337de5244ec9f06a9cff5c
Application-drop-up <msts.oo0131@gmail.com>
2026-03-07 16:47:30 +0900

    change hash router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppRoutes } from "./app/routes/AppRoutes.tsx";
 
 export default function App(): React.ReactElement {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
