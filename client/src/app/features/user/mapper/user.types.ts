863ac2022636da90ac04912b039af57107a3f768
Application-drop-up <msts.oo0131@gmail.com>
2026-07-11 16:29:06 +0900

    feat(user): add UserCreateContainer and wire /users/new route

---
+++ client/src/app/features/user/mapper/user.types.ts
@@ -0,0 +1,11 @@
+import type { CreateUserFormValues } from "../types";
+
+export const DEFAULT_CREATE_USER_FORM_VALUES: CreateUserFormValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  ageRange: "teens",
+  subscriptionTier: "free",
+  subscriptionExpiresAt: null,
+};
